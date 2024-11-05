c0e3141f44befa402cb4481828cacc24ed5f3df5
Leonardo da Motta Chies <g00gl3dzn@gmail.com>
2024-11-04 21:04:15 -0300

    Adiciona models de Despesa e PerfilEconomia

--- Ola_Django-main/ola_django/primeiro_app/models.py
+++ Ola_Django-main/ola_django/primeiro_app/models.py
@@ -25,3 +25,34 @@ class InteracoesPessoa(models.Model):
 
     def __str__(self):
         return f"Interação com {self.pessoa.nome} em {self.data_hora}"
+    
+class Despesa(models.Model):
+    CATEGORIAS = [
+        ('Fixa', 'Despesa Fixa'),
+        ('Variavel', 'Despesa Variável'),
+        ('Geral', 'Despesa Geral'),
+        ('Investimento', 'Investimento')
+    ]
+
+    categoria = models.CharField(max_length=15, choices=CATEGORIAS)
+    valor = models.FloatField()
+    descricao = models.CharField(max_length=255, blank=True, null=True)
+    data = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.categoria} - {self.valor}"
+
+class PerfilEconomia(models.Model):
+    TIPOS_ECONOMIA = [
+        ('Passiva', 'Economia Passiva'),
+        ('Agressiva', 'Economia Agressiva')
+    ]
+    
+    tipo = models.CharField(max_length=10, choices=TIPOS_ECONOMIA)
+    porcentagem_despesa_fixa = models.FloatField()
+    porcentagem_despesa_variavel = models.FloatField()
+    porcentagem_despesa_geral = models.FloatField()
+    porcentagem_investimento = models.FloatField()
+
+    def __str__(self):
+        return f"{self.tipo} - Despesas F: {self.porcentagem_despesa_fixa}% Var: {self.porcentagem_despesa_variavel}% Geral: {self.porcentagem_despesa_geral}% Invest: {self.porcentagem_investimento}%"
